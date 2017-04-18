40a407229b9b0768dfc3aa120c3e1faa4c17f4ad
ne-peer <mileage0110@gmail.com>
2017-04-18 14:50:08 +0900

    [update] scraping

--- scripts/pixiv-scraping.js
+++ scripts/pixiv-scraping.js
@@ -1,25 +1,32 @@
 /**
  * Description:
- *   スクレイピング
+ *   pixiv百科事典スクレイピング
+ * 　整形はまだ甘い･･･。
  * 
  * Commands:
- *   dia (free word)の<絵|イラスト>
+ *   dia (free word)って<誰|だれ>
  */
 var client = require('cheerio-httpcli');
 
-const SELECTOR = 'title';
+// 引き抜く箇所のセレクタ
+const SELECTOR_SUMMARY = '#content div.summary';
+const SELECTOR_DESCRIPTION = '#content #h2_0+p';
 
 module.exports = robot => {
-    robot.respond(/(.+)の(絵|イラスト)/i, msg => {
+    robot.respond(/(.+)って(何|なに)/i, msg => {
         const query = msg.match[1];
 
-        // Googleで「node.js」について検索する。
-        client.fetch('http://www.google.com/search', { q: query }, function (err, $, res) {
+        const requestUri = encodeURI('http://dic.pixiv.net/a/' + query);
+        console.log(requestUri);
 
+        // ピクシブ百科事典で検索する
+        client.fetch(requestUri, function (err, $, res) {
             if (!err) {
-                const content = $(SELECTOR).text();
-                msg.send(content);
+                const summary = $(SELECTOR_SUMMARY).text();
+                const description = $(SELECTOR_DESCRIPTION).text();
+                msg.send(summary + '\n' + description + '\n\nですわ！');
             } else {
+                console.log(err);
                 msg.send('失敗しましたわ･･･。');
             }
 
