0c0990b9d9d70aa449068aa17fd28caf57e01a7d
ne-peer <mileage0110@gmail.com>
2017-04-19 18:04:29 +0900

    add limit

--- scripts/tumblr-tag.js
+++ scripts/tumblr-tag.js
@@ -9,24 +9,30 @@
 const request = require('request');
 
 const uri = 'https://api.tumblr.com/v2/tagged';
+const limit = 5;
 
 module.exports = robot => {
     robot.respond(/(.+)の(絵|イラスト)/i, msg => {
         const query = msg.match[1];
-        const apiKey = process.env.HUBOT_TUMBRL_CONSUMER_KEY;
+        const apiKey = "O0zjjGR8YTA0VEXSr9TY2L0PXCxlIdASbCSQLHNTc8Kk2ymCU0";
 
-        request(uri + '?tag=' + query + '&api_key=' + apiKey, (err, response, body) => {
+        const requestUrl = uri + '?tag=' + query + '&limit=' + limit + '&api_key=' + apiKey;
+        request(requestUrl, (err, response, body) => {
             if (err) {
                 console.log(err);
                 msg.send('失敗しましたわ･･･。');
-            } else if (data.response.length < 1) {
+                return;
+            }
+            
+            const arrayContents = JSON.parse(body);
+            if (arrayContents.response.length < 1) {
                 msg.send('見つかりませんでしたわ。');
+                return;
             }
 
             // 画像コンテンツの投稿URLだけリスト化
-            const data = JSON.parse(json_data);
             let photoPostList = [];
-            for (let content of data.response) {
+            for (let content of arrayContents.response) {
                 if (content.type === "photo") {
                     photoPostList.push(content.post_url);
                 }
