0e5843f4537be0317b2a1a1a9f01613b0b2a5e5e
ne-peer <mileage0110@gmail.com>
2017-05-17 15:33:44 +0900

    [update] cg-image test関数削除

--- scripts/imas-cg-image.js
+++ scripts/imas-cg-image.js
@@ -25,47 +25,43 @@ module.exports = robot => {
                 return;
             }
 
-            // wip
+            const arrayDb = JSON.parse(body);
+
+            let matchList = [];
+            for (let hash in arrayDb) {
+                let oneIdol = arrayDb[hash];
+
+                // nameにqueryが含まれていたら追加
+                let name = oneIdol.name;
+                if (name.match(query)) {
+                    matchList.push(oneIdol);
+                }
+            }
+
+            // 1件も一致しなければ終了
+            if (matchList.length < 1) {
+                msg.send('見つかりませんわ･･･。');
+                return;
+            }
+
+            // ランダムに1つ取得
+            let idol = matchList[Math.floor(Math.random() * matchList.length)];
+
+            // スクレイピング
+            let message = '';
+            cheerio.fetch(IMAS_CG_DB_SITE + idol.id, function (err, $, res) {
+                if (!err) {
+                    const imgUrl = $(SELECTOR_IMAGE_LINK).attr("content");
+
+                    message = idol.name + '\n' + imgUrl;
+                } else {
+                    message = '失敗しましたわ･･･。(2)\n```' + err + '```';
+                }
+
+                msg.send(message);
+                return;
+            });
         });
     });
 
-    robot.respond(/(.+)の(test)/i, msg => {
-        const query = msg.match[1];
-
-        const json = '{"0dabb79ff64691111a0abae2ffed01ce":{"id":"1000101","hash":"0dabb79ff64691111a0abae2ffed01ce","name":"島村卯月","idol_id":1,"next_id":"1100102","next_hash":"798ed3093c682f20234934cef65c2106","id_family":["1000101","1100102"],"hash_family":["0dabb79ff64691111a0abae2ffed01ce","798ed3093c682f20234934cef65c2106"]},"798ed3093c682f20234934cef65c2106":{"id":"1100102","hash":"798ed3093c682f20234934cef65c2106","name":"島村卯月+","idol_id":1,"prev_id":"1000101","prev_hash":"0dabb79ff64691111a0abae2ffed01ce","id_family":["1000101","1100102"],"hash_family":["0dabb79ff64691111a0abae2ffed01ce","798ed3093c682f20234934cef65c2106"]},"250645af5bca76120314f21e69b0caca":{"id":"1213201","hash":"250645af5bca76120314f21e69b0caca","name":"[2ndｱﾆﾊﾞｰｻﾘｰ]島村卯月","idol_id":1,"next_id":"1313202","next_hash":"5ebb3e7bbe7fd6a46a5bfc3eab50097f","id_family":["1213201","1313202"],"hash_family":["250645af5bca76120314f21e69b0caca","5ebb3e7bbe7fd6a46a5bfc3eab50097f"],"id_sibling":["1213301","1313302","1213401","1313402"],"hash_sibling":["61bde20d527ff4744e91729ffbd67058","421e19ae69094b30d5edd8fa97e4d7bf","7d7fa151dcc1e44e6aaeadb3fd14eab7","09b1b846d0f89dc251b0bcac4e497e14"]}}';
-
-        let arrayDb = JSON.parse(json);
-
-        let matchList = [];
-        for (let hash in arrayDb) {
-            let oneIdol = arrayDb[hash];
-
-            // nameにqueryが含まれていたら追加
-            let name = oneIdol.name;
-            if (name.match(query)) {
-                matchList.push(oneIdol);
-            }
-        }
-
-        if (matchList.length < 1) {
-            msg.send('not found');
-            return;
-        }
-
-        // ランダムに1つ取得
-        let idol = matchList[Math.floor(Math.random() * matchList.length)];
-
-        // スクレイピング
-        cheerio.fetch(IMAS_CG_DB_SITE + idol.id, function (err, $, res) {
-            if (!err) {
-                const imgUrl = $(SELECTOR_IMAGE_LINK).attr("content");
-
-                msg.send(idol.name + '\n' + imgUrl);
-                return;
-            } else {
-                msg.send('失敗しましたわ･･･。(2)\n```' + err + '```');
-                return;
-            }
-        });
-    });
 };
