1c19b3db06103b70182f2c1002f2c8d40e68b516
ne-peer <mileage0110@gmail.com>
2017-05-24 13:11:39 +0900

    [update] update script

--- utility_scripts/dia-update.js
+++ utility_scripts/dia-update.js
@@ -13,18 +13,24 @@ module.exports = robot => {
         const target = msg.match[1];
 
         const updateRecipe = [
-            'cd $(cd $(dirname $0) && pwd)/../../' + target + '/dia',
+            'cd $(cd $(dirname $0) && pwd)/../../../' + target + '/dia',
             'git pull',
             'sh slack-token.sh'
         ];
 
         for (let step of updateRecipe) {
+            let isFailure = false;
             exec(step, function (err, stdout, stderr) {
                 if (err) {
                     msg.send('update failure!\n```' + err + '```');
-                    return;
+                    isFailure = true;
                 }
             });
+
+            if (isFailure === true) {
+                // 中断
+                return;
+            }
         }
 
         msg.send('`' + target + '` update success!');
