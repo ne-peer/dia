1503d23d16fbfa83fd9efce06b8d847c7d082ea1
ne-peer <mileage0110@gmail.com>
2017-08-30 17:06:53 +0900

    [add] pixivイメージスクレイピング

---
+++ scripts/pixiv-img-scraping.js
@@ -0,0 +1,105 @@
+/**
+ * Description:
+ *   pixivイメージ検索スクレイピング
+ * 
+ * Commands:
+ *   dia <かわいい|カワイイ|可愛い>(free word)
+ */
+const phantom = require('phantom');
+const cheerio = require('cheerio');
+
+// Phantom settings
+const waitTimeMsec = 10000;
+const ua = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.71 Safari/537.36';
+
+// Serach settings
+const searchUrl = 'https://www.pixiv.net/search.php?word=';
+const responseBaseUrl = 'http://embed.pixiv.net/decorate.php?illust_id=';
+const addQualitySuffix = true;
+
+// スクレイピング処理
+const scraper = (msg, query) => {
+    let _ph;
+    let _page;
+
+    phantom.create().then(ph => {
+        _ph = ph;
+        return _ph.createPage();
+    }).then(page => {
+        _page = page;
+
+        // warn: Using page.settings = ...; is not supported. Use page.property('settings', ...) instead. See the README file for more examples of page#property.
+        _page.settings = {
+            userAgent: ua
+        };
+
+        // url
+        let url = searchUrl + encodeURI(query);
+        if (addQualitySuffix === true) {
+            url += encodeURI(' 100user');
+        }
+
+        // send progress
+        msg.send(query + 'を探していますわ..');
+
+        return _page.open(url);
+    }).then(status => {
+        console.log(status);
+
+        // console log setup
+        _page.property('onConsoleMessage', function (msg) {
+            console.log('console: ' + msg);
+        });
+
+        let response = [];
+
+        // page load waiting
+        setTimeout(function () {
+
+            // main scraping
+            _page.evaluate(function () {
+                return document.querySelector('html').innerHTML;
+            }).then(function (html) {
+                console.log('html load complete.');
+
+                const $ = cheerio.load(html);
+                const dataItems = $('#js-mount-point-search-result-list').attr('data-items');
+
+                try {
+                    const resultJson = JSON.parse(dataItems);
+                } catch (e) {
+                    msg.send(`検索に失敗しましたわ･･･。\n待ち時間をもう少し長く設定してください。現在は${waitTimeMsec}msですわ。`);
+                    return;
+                }
+
+                const links = [];
+                for (let key in resultJson) {
+                    const url = responseBaseUrl + resultJson[key]['illustId']
+                    links.push(url);
+                }
+
+                console.log(links);
+
+                _page.close();
+                _ph.exit();
+
+                if (links.length > 0) {
+                    msg.send('これですわ！\n' + msg.random(links));
+                } else {
+                    msg.send('見つかりませんでしたわ･･･。');
+                }
+            });
+        }, waitTimeMsec);
+
+    }).catch(e => console.log(e));
+};
+
+module.exports = robot => {
+    robot.respond(/(かわいい|カワイイ|可愛い)(.+)/i, msg => {
+        const query = msg.match[2];
+
+        // メイン処理
+        scraper(msg, query);
+    });
+};
+
