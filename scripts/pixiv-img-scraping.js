30b04ceb47c415e9d5098b206b8ce8c9a00cd215
ne-peer <mileage0110@gmail.com>
2018-02-07 15:53:16 +0900

    [update] pixiv画像検索をphantomからpuppeteerに変更

--- scripts/pixiv-img-scraping.js
+++ scripts/pixiv-img-scraping.js
@@ -1,137 +1,59 @@
 /**
  * Description:
- *   pixivイメージ検索スクレイピング
+ *   Search images on pixiv.net
  * 
  * Commands:
- *   dia <かわいい|カワイイ|可愛い>(free word)
+ *   @botname <かわいい>keyword
  */
-const phantom = require('phantom');
+const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
-
-// Phantom settings
-const timeoutMsec = 30000;
-const ua = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.71 Safari/537.36';
 
 // Serach settings
 const searchUrl = 'https://www.pixiv.net/search.php?word=';
-const responseBaseUrl = 'http://embed.pixiv.net/decorate.php?illust_id=';
-const addQualitySuffix = true;
+const decoUrl = 'http://embed.pixiv.net/decorate.php?illust_id=';
 
-// スクレイピング処理
-const scraper = (msg, query) => {
-    let _ph;
-    let _page;
+/**
+ * クローラ
+ * 
+ * @param {string} keyword 検索ワード
+ */
+const crawler = async keyword => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-    phantom.create().then(ph => {
-        _ph = ph;
-        return _ph.createPage();
-    }).then(page => {
-        _page = page;
+    // 検索実行
+    await page.goto(searchUrl + keyword);
+    const html = await page.$eval('html', elem => { return elem.innerHTML });
+    await browser.close();
 
-        // warn: Using page.settings = ...; is not supported. Use page.property('settings', ...) instead. See the README file for more examples of page#property.
-        _page.settings = {
-            userAgent: ua
-        };
-
-        // url
-        let url = searchUrl + encodeURI(query);
-        if (addQualitySuffix === true) {
-            // 検索クエリにお気に入りユーザ数が多いことを示すタグを追記して検索する
-            url += encodeURI(' 100user');
-        }
-
-        // send progress
-        msg.send(query + 'を探していますわ..');
-
-        return _page.open(url);
-    }).then(status => {
-        // console log setup
-        _page.property('onConsoleMessage', function (msg) {
-            console.log('console: ', msg);
-        });
-
-        // site open status logging
-        console.log('Opened url?: ', status);
-
-        // timeout promise
-        const timeoutTimer = function () {
-            return new Promise(function (resolve, reject) {
-                setTimeout(function () {
-                    Promise.reject(new Error('Html load timeouted. Wait time = ' + timeoutMsec + 'ms.'));
-                }, timeoutMsec);
-            });
-        }
-
-        // scraping loop promise
-        const selectorFetchLoop = function () {
-            return new Promise(function (resolve, reject) {
-                let dataItems = [];
-
-                // 取得できるまでevaluateループ
-                while (dataItems.length < 1 || typeof dataItems === "undefined") {
-                    dataItems = _page.evaluate(function () {
-                        return document.querySelector('html').innerHTML;
-                    }).then(function (html) {
-                        const $ = cheerio.load(html);
-                        return $('#js-mount-point-search-result-list').attr('data-items');
-                    }).catch(function(e) {
-                        console.log('Is not matches for selector.', e);
-                    });
-                }
-
-                resolve(dataItems);
-            });
-        };
-
-        // 実行
-        Promise.race([
-            // タイムアウトタイマーとスクレイピングを並行処理
-            timeoutTimer(),
-            selectorFetchLoop()
-        ]).then(function (dataItems) {
-            try {
-                resultJson = JSON.parse(dataItems);
-            } catch (e) {
-                throw new Error('Invalid json. ' + dataItems);
-            }
-
-            const links = [];
-            for (let key in resultJson) {
-                const url = responseBaseUrl + resultJson[key]['illustId']
-                links.push(url);
-            }
-
-            console.log(links);
-
-            _page.close();
-            _ph.exit();
-
-            if (links.length > 0) {
-                console.log('ok');
-                msg.send('これですわ！\n' + msg.random(links));
-            } else {
-                console.log('result is not exist');
-                msg.send('見つかりませんでしたわ･･･。');
-            }
-        }).catch(function (e) {
-            // aborted
-            msg.send('検索に失敗しましたわ･･･。(2)\n```' + e + '```');
-        });
-
-    }).catch(e => {
-        // exception
-        msg.send('失敗しましたわ･･･。(1)\n```' + e + '```');
-    });
-    
-    return;
+    return html;
 };
+
+/**
+ * スクレイピング
+ * 
+ * @param {string} html
+ */
+const scraper = async html => {
+    const $ = cheerio.load(html);
+    const json = $('#js-mount-point-search-result-list').attr('data-items');
+    return JSON.parse(json).map(obj => {
+        return decoUrl + obj['illustId'];
+    });
+}
 
 module.exports = robot => {
-    robot.respond(/(かわいい|カワイイ|可愛い)(.+)/i, msg => {
-        const query = msg.match[2];
+    robot.respond(/(かわいい)(.+)/i, msg => {
+        (async () => {
+            const query = await msg.match[2] + ' 1000user';
+            await msg.send(`${query}を探していますわ...`);
+            const html = await crawler(query);
+            const links = await scraper(html);
 
-        // メイン処理
-        scraper(msg, query);
+            // レスポンス
+            await msg.send('これですわ！\n' + msg.random(links));
+        })().catch(err => {
+            msg.send('```' + err + '```');
+        });
     });
 };
-
